5a1bb597f2b1c9d985370187d32ea3a5a2b5086a
harrrrrrrrr3 <158248262+harrrrrrrrr3@users.noreply.github.com>
2024-01-31 13:46:52 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,439 @@
+let questions = [
+    {
+        "volume": 1,
+        "poem": "我等は 姿無きが故に\nそれを畏れ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51RqypO2UYL.jpg"
+    },
+    {
+        "volume": 2,
+        "poem": "人が希望を持ちえるのは\n死が目に見えぬものであるからだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/518dzw9souL.jpg"
+    },
+    {
+        "volume": 3,
+        "poem": "もし わたしが雨だったなら\nそれが永遠に交わることのない\n空と大地を繋ぎ留めるように\n誰かの心を繋ぎ留めることができただろうか",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51opRvyI0RL.jpg"
+    },
+    {
+        "volume": 4,
+        "poem": "ぼくたちは ひかれあう\n水滴のように 惑星のように\nぼくたちは 反発しあう\n磁石のように 肌の色のように",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51MytU8AzUL.jpg"
+    },
+    {
+        "volume": 5,
+        "poem": "剣を握らなければ おまえを守れない\n剣を握ったままでは おまえを抱き締められない",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51WyH1kc0UL.jpg"
+    },
+    {
+        "volume": 6,
+        "poem": "そう、 我々に運命などない\n無知と恐怖にのまれ\n足を踏み外したものたちだけが\n運命と呼ばれる濁流の中へと\n堕ちてゆくのだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/5138X5MhhOL.jpg"
+    },
+    {
+        "volume": 7,
+        "poem": "我々は涙を流すべきではない\nそれは心に対する肉体の敗北であり\n我々が心というものを\n持て余す存在であるということの\n証明にほかならないからだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51YDyUjg-5L.jpg"
+    },
+    {
+        "volume": 8,
+        "poem": "錆びつけば 二度と突き立てられず\n掴み損なえば我が身を裂く\n\nそう 誇りとは\n刃に似ている",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51anrTxwfKL.jpg"
+    },
+    {
+        "volume": 9,
+        "poem": "ああ おれたちは皆\n眼をあけたまま\n空を飛ぶ夢を見てるんだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51xH-oX0qQL.jpg"
+    },
+    {
+        "volume": 10,
+        "poem": "俺達は 手を伸ばす\n雲を払い 空を貫き\n月と火星は掴めても\n真実には まだ届かない",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/512rKfQOsiL.jpg"
+    },
+    {
+        "volume": 11,
+        "poem": "届かぬ牙に 火を灯す\nあの星を見ずに済むように\nこの吭を裂いて しまわぬように",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51EaW1VRfdL.jpg"
+    },
+    {
+        "volume": 12,
+        "poem": "我々が岩壁の花を美しく思うのは\n我々が岩壁に足を止めてしまうからだ\n悚れ無き その花のように\n空へと踏み出せずにいるからだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51Z5N5ssgcL.jpg"
+    },
+    {
+        "volume": 13,
+        "poem": "誇りを一つ捨てるたび\n我等は獣に一歩近付く\n心を一つ殺すたび\n我等は獣から一歩遠退く",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51gWrjmCcoL.jpg"
+    },
+    {
+        "volume": 14,
+        "poem": "軋む軋む 浄罪の塔\n光のごとくに 世界を貫く\n揺れる揺れる 背骨の塔\n堕ちてゆくのは ぼくらか 空か",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51JMDUCEbmL.jpg"
+    },
+    {
+        "volume": 15,
+        "poem": "ぼくは ただ きみに\nさよならを言う練習をする",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51qEeyQxXyL.jpg"
+    },
+    {
+        "volume": 16,
+        "poem": "降り頻る太陽の鬣が\n薄氷に残る足跡を消してゆく\n\n欺かれるを恐れるな\n世界はすでに欺きの上にある",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/510%2BaF4mU-L.jpg"
+    },
+    {
+        "volume": 17,
+        "poem": "血のように赤く\n骨のように白く\n孤独のように赤く\n沈黙のように白く\n獣の神経のように赤く\n神の心臓のように白く\n溶け出す憎悪のように赤く\n凍てつく傷歎のように白く\n夜を食む影のように赤く\n月を射抜く吐息のように\n白く輝き 赤く散る",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51uI7hQysTL.jpg"
+    },
+    {
+        "volume": 18,
+        "poem": "あなたの影は 密やかに\n行くあての無い 毒針のように\n私の歩みを縫いつける\n\nあなたの光は しなやかに\n給水塔を打つ 落雷のように\n私の命の源を断つ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51SLS0-hpBL.jpg"
+    },
+    {
+        "volume": 19,
+        "poem": "そう、 何ものも わたしの世界を 変えられはしない",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51KSmVqZ0pL.jpg"
+    },
+    {
+        "volume": 20,
+        "poem": "美しきを愛に譬ふのは\n愛の姿を知らぬ者\n\n醜きを愛に譬ふのは\n愛を知ったと驕る者",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51V2DSI3QvL.jpg"
+    },
+    {
+        "volume": 21,
+        "poem": "この世のすべては\nあなたを追い詰める為にある",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51qLutWHySL.jpg"
+    },
+    {
+        "volume": 22,
+        "poem": "我等の世界に意味など無く\nそこに生きる我等にも 意味など無い\n無意味な我等は 世界を想う\nそこに意味は無いと知ることにすら\n意味など無いというのに",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51fIZ%2BINzWL.jpg"
+    },
+    {
+        "volume": 23,
+        "poem": "俺たちは滝の前の魚\n俺たちは籠の中の虫\n\n俺たちは波濤の残骸\n髑髏の錫杖\n力の奔流 それを呑む鯨\n\n俺たちは五本角の雄牛\n俺たちは火を吹く怪物\n泣き叫ぶ子供\n\nああ 俺たちは\n月光に毒されている",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51usWVb7RAL.jpg"
+    },
+    {
+        "volume": 24,
+        "poem": "どいつもこいつも、\nぶっ壊れちまえ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51gn5YMKzBL.jpg"
+    },
+    {
+        "volume": 25,
+        "poem": "我々は皆\n生まれながらにして死んでいる\n終焉は常に\n始まりの前から そこに在るのだ\n\n生きることが\n何かを知り続けることならば\n我々が最後に知るものこそが終焉であり\n終焉をついに見出し\n完全に知ることこそが\n即ち死なのだ\n\n我々は何かを知ろうとしてはならない\n死を超越できぬ者は\n何ものも知ろうとしてはならないのだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51%2B3gTTvlgL.jpg"
+    },
+    {
+        "volume": 26,
+        "poem": "私の胸に深く突き刺さるその声は\n鳴り止まぬ歓声に似ている",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/510%2Beoo1hHL.jpg"
+    },
+    {
+        "volume": 27,
+        "poem": "私達\n一つとして\n混じりあうものはない\n二つとして\n同じ貌をしていない\n三つ目の\n瞳を持たぬばかりに\n四つ目の\n方角に希望はない\n五つ目は\n心臓の場所にある",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51e4AFNuh5L.jpg"
+    },
+    {
+        "volume": 28,
+        "poem": "主よ、我々は\n孔雀を見るような目つきで\nあなたを見る\n\nそれは期待と、渇仰と\n恐怖に似た底知れぬものに\n縁どられているのだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51jH7YhhpbL.jpg"
+    },
+    {
+        "volume": 29,
+        "poem": "ただ執拗に 飾り立てる\n切り落とされると知りながら\nただ執拗に 磨き上げる\n切り落とされると知りながら\n恐ろしいのだ 恐ろしいのだ\n切り落とされる その時が\n切り落とされた その髪は\n死んだあなたに 似てしまう\n髪も爪も みな宝物のように\n美しく飾り立てるのに\nなぜ自らの体から切り離されただけで\n汚く不気味なものとなってしまうのだろう\n答えは簡単\nそれらは全て\n自らの死した姿に ほかならないからだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51ygxIAJO8L.jpg"
+    },
+    {
+        "volume": 30,
+        "poem": "その疵深し、海淵の如し\n\nその罪赤し、死して色無し",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51FXWgE4ZNL.jpg"
+    },
+    {
+        "volume": 31,
+        "poem": "世界一嫌いだと言ってくれ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51N8n3buSoL.jpg"
+    },
+    {
+        "volume": 32,
+        "poem": "王は駆ける\n\n影を振り切り\n鎧を鳴らし\n骨を蹴散らし\n血肉を啜り\n軋みを上げる\n心を潰し\n\n独り踏み入る\n遙か彼方へ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/517V9aRNKTL.jpg"
+    },
+    {
+        "volume": 33,
+        "poem": "俺達は虫\n\n不揮発性の\n悪意の下で\n這い回る蠕虫\n\n首をもたげる\n月より高く\n\n憐れなお前等が\n見えなくなるまで",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51SYapHjbqL.jpg"
+    },
+    {
+        "volume": 34,
+        "poem": "私に翼をくれるなら\n私はあなたのために飛ぼう\n\nたとえば この 大地のすべてが\n水に沈んでしまうとしても\n\n私に剣をくれるなら\n私はあなたのために立ち向かおう\n\nたとえば この 空のすべてが\nあなたを光で射抜くとしても",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51-a7S2Y50L.jpg"
+    },
+    {
+        "volume": 35,
+        "poem": "産まれ堕ちれば、\n死んだも同然",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51-FUZT6PbL.jpg"
+    },
+    {
+        "volume": 36,
+        "poem": "信じるのは、まだ早い",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51HkGnVcZrL.jpg"
+    },
+    {
+        "volume": 37,
+        "poem": "人を美しいとは思わないけれど\n花を美しいとは思う\n\n人の姿が花に似るのは\nただ斬り裂かれて倒れる時だ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51Qw6BeeSYL.jpg"
+    },
+    {
+        "volume": 38,
+        "poem": "恐れることは ただ一つ\n\n恐れを知らぬ 戦士と為ること",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51iPQL6fKWL.jpg"
+    },
+    {
+        "volume": 39,
+        "poem": "愆つは、人\n\n殺すは、魔",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51uRP-YO44L.jpg"
+    },
+    {
+        "volume": 40,
+        "poem": "心在るが故に妬み\n心在るが故に喰らい\n心在るが故に奪い\n心在るが故に傲り\n心在るが故に惰り\n心在るが故に怒り\n心在るが故に\nお前のすべてを欲する",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51TNEsZRaZL.jpg"
+    },
+    {
+        "volume": 41,
+        "poem": "失くしたものを\n奪い取る\n血と肉と骨と\nあとひとつ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/517ZBhsgP8L.jpg"
+    },
+    {
+        "volume": 42,
+        "poem": "犠牲無き世界など ありはしない\n気付かないのか\n我々は\n血の海に 灰を浮かべた地獄の名を\n仮に世界と\n呼んでいるのだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51eQwropssL.jpg"
+    },
+    {
+        "volume": 43,
+        "poem": "腐敗は我が友\n夜は我が僕\n鴉にこの身を啄ませながら\n楡の館でお前を待つ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51bFLEoRoqL.jpg"
+    },
+    {
+        "volume": 44,
+        "poem": "人は皆すべからく悪であり\n自らを正義であると錯覚する為には\n己以外の何者かを 己以上の悪であると\n錯覚するより 他にないのだ\n\n確信した正義とは、悪である\n正義が正義たり得る為には\n常に自らの正義を疑い続けなければならない",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51jH1QKEJdL.jpg"
+    },
+    {
+        "volume": 45,
+        "poem": "伏して生きるな、\n立ちて死すべし",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51NOWEAz9tL.jpg"
+    },
+    {
+        "volume": 46,
+        "poem": "不幸を知ることは\n怖ろしくはない\n怖ろしいのは\n過ぎ去った幸福が\n戻らぬと知ること",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51d5crOQzsL.jpg"
+    },
+    {
+        "volume": 47,
+        "poem": "君が明日 蛇となり\n人を喰らい 始めるとして\n人を喰らった その口で\n僕を愛すと 咆えたとして\n僕は果して 今日と同じに\n君を愛すと 言えるだろうか",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51I7fAafI1L.jpg"
+    },
+    {
+        "volume": 48,
+        "poem": "人は皆、猿のまがいもの\n神は皆、人のまがいもの",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51PJLnayORL.jpg"
+    },
+    {
+        "volume": 49,
+        "poem": "僕は、ついてゆけるだろうか\n\n君のいない世界のスピードに",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51ibh6pA7kL.jpg"
+    },
+    {
+        "volume": 50,
+        "poem": "時は常に背後から迫り\n唸りを上げて眼前に流れ去る\n\n踏み止まれ\n時がお前を 美しい世界へ押し流そうと\nどれほど牙を剥こうとも\n\n前を見るな\nお前の希望は 背後に迫る\n冥冥たる濁流の中にしかない",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51iHhS5l4LL.jpg"
+    },
+    {
+        "volume": 51,
+        "poem": "あたしの心に 指を入れないで",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51MqbEQeA0L.jpg"
+    },
+    {
+        "volume": 52,
+        "poem": "一緒に数えてくれるかい\n君についた\n僕の歯型を",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/518QKs9hSHL.jpg"
+    },
+    {
+        "volume": 53,
+        "poem": "僕が こんなにも若く\nこんなにも未熟であるということが\n老いさらばえ\n完全無欠である大人達には\nどうにも許し難いことのようなのだ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51MjQd16HrL.jpg"
+    },
+    {
+        "volume": 54,
+        "poem": "変わらぬものは 心だと\n言えるのならば それが強さ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/516fHQXxN8L.jpg"
+    },
+    {
+        "volume": 55,
+        "poem": "一歩踏み出す 二度と戻れぬ\n三千世界の 血の海へ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51m05ylZ0mL.jpg"
+    },
+    {
+        "volume": 56,
+        "poem": "軍勢ゆきゆきて喇叭を吹く\n耳鳴り止まず星屑のごとく\n軍靴の轟き雷鳴のごとく",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51%2BbaVSGXqL.jpg"
+    },
+    {
+        "volume": 57,
+        "poem": "散りて二度とは 咲かずとも\n炎のごとくに 散るぞ美し",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51GHaAc5xeL.jpg"
+    },
+    {
+        "volume": 58,
+        "poem": "魂 燃え立つ\n天の降るとも",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51pVWKpjn4L.jpg"
+    },
+    {
+        "volume": 59,
+        "poem": "戦いこそすべて",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51%2BLvjqCWcL.jpg"
+    },
+    {
+        "volume": 60,
+        "poem": "罪無きあなたは 太陽のよう\n罪深きあなたも 太陽のよう",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/515p2aUOcIL.jpg"
+    },
+    {
+        "volume": 61,
+        "poem": "私が 世界には危険が満ちていると信じ\nその危険からお前を護りたいと願うのは\n私の中にその危険と同質の\n衝動があるからに ほかならない",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51ffDt5wLgL.jpg"
+    },
+    {
+        "volume": 62,
+        "poem": "己の生に\n抗い続ける\n己の心に\n牙ある限り",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51jjXAgVqkL.jpg"
+    },
+    {
+        "volume": 63,
+        "poem": "生きることと 生かされることに\n違いは無い\n\n死ぬことと 殺されることに\n違いが無いように",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51KaO6w1SAL.jpg"
+    },
+    {
+        "volume": 64,
+        "poem": "美しさとは、\nそこに何もないこと",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/511nY%2BvvxeL.jpg"
+    },
+    {
+        "volume": 65,
+        "poem": "すきだよ\nしぬほど",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51WZN7IasiL.jpg"
+    },
+    {
+        "volume": 66,
+        "poem": "断ち斬るものは\n命だけかい",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51ZEBwU%2Bp9L.jpg"
+    },
+    {
+        "volume": 67,
+        "poem": "御先\n真黒\n真逆様",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/519NKOQgJDL.jpg"
+    },
+    {
+        "volume": 68,
+        "poem": "毒々しくって、\n眩々するだろ？",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51NGOCOfnkL.jpg"
+    },
+    {
+        "volume": 69,
+        "poem": "銃弾、鉤爪、軍旗、刀剣、\n五本指折り\nお前を待つ",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/510oxO6tVlL.jpg"
+    },
+    {
+        "volume": 70,
+        "poem": "痛みはない\nその天秤から\n目を逸らせぬ事以外に",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/510r8PyyLML.jpg"
+    },
+    {
+        "volume": 71,
+        "poem": "吾子の手かわいや さまよう手\n吾をもとめて 流離うて\n寄らば離れる 手をとって\n引きて歩もう 沙汰の果て",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51jmle6mYhL.jpg"
+    },
+    {
+        "volume": 72,
+        "poem": "言葉に姿があったなら\n暗闇に立つきみに届きはしないだろう",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51dfD2zQ2%2BL.jpg"
+    },
+    {
+        "volume": 73,
+        "poem": "牙より滴る火は消えず\n刃立つ野を焼き払い\n友の姿を炙り出す",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51WPDf1DjBL.jpg"
+    },
+    {
+        "volume": 74,
+        "poem": "我等は 姿無くとも\n歩みは止めず",
+        "imageUrl": "https://images-fe.ssl-images-amazon.com/images/I/51tc99r9KpL.jpg"
+    }
+]
+
+const questionsLength = questions.length;
+let quizIndex = 0;
+let score = 0;
+
+function questionShuffle(array) {
+    for (let i = (array.length - 1); 0 < i; i--) {
+
+        // 0〜(i+1)の範囲で値を取得
+        let r = Math.floor(Math.random() * (i + 1));
+
+        // 要素の並び替えを実行
+        let tmp = array[i];
+        array[i] = array[r];
+        array[r] = tmp;
+    }
+    return array;
+};
+questionShuffle(questions);
+
+const button = document.getElementsByTagName('button');
+const buttonLength = button.length;
+
+const setupQuiz = () => {
+    document.getElementById('question').innerText = questions[quizIndex].poem;
+    let buttonIndex = 0;
+    const answerPos = Math.floor(Math.random() * 4);
+    while (buttonIndex < buttonLength) {
+        if (buttonIndex === answerPos) {
+            button[buttonIndex].textContent = questions[quizIndex].volume;
+        } else {
+            let wrongVolume = questions[quizIndex].volume;
+            while (wrongVolume === questions[quizIndex].volume) {
+                wrongVolume = Math.floor(Math.random() * 74) + 1;
+            }
+            button[buttonIndex].textContent = wrongVolume;
+        }
+        buttonIndex++;
+    }
+}
+
+setupQuiz();
+
+const clickHandler = (e) => {
+    if (String(questions[quizIndex].volume) === e.target.textContent) {
+        window.alert("正解！");
+        score++;
+    } else {
+        window.alert("不正解！\n正解は" + String(questions[quizIndex].volume) + "巻");
+    }
+
+    quizIndex++;
+
+    if (quizIndex < questionsLength) {
+        setupQuiz();
+    } else {
+        window.alert('終了！あなたの正解数は' + score + '/' + questionsLength + 'です！');
+    }
+}
+
+let handlerIndex = 0;
+while (handlerIndex < buttonLength) {
+    button[handlerIndex].addEventListener('click', (e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
